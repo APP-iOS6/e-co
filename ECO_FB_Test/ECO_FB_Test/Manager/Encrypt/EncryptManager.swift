80017f6afd2b9de81d87a16ff04a5409af137820
Jaemin <ghdckdwn8456@gmail.com>
2024-10-19 16:37:05 +0900

    [feat] 암호화 기능 구현(CryptoSwift 추가))

---
+++ ECO_FB_Test/ECO_FB_Test/Manager/Encrypt/EncryptManager.swift
@@ -0,0 +1,55 @@
+//
+//  EncryptManager.swift
+//  ECO_FB_Test
+//
+//  Created by Jaemin Hong on 10/19/24.
+//
+
+import Foundation
+import CryptoSwift
+
+final class EncryptManager {
+    static let shared: EncryptManager = EncryptManager()
+    private let key: String = "2b8v524k81fn9ahtg9x0q136dw377yzr"
+    private let iv: String = "grq53nxxns5u8kdr"
+    
+    private init() {}
+    
+    func encrypt(_ data: String) throws -> String {
+        guard !data.isEmpty else { return "" }
+        
+        do {
+            let result = try getAESObject().encrypt(data.bytes).toBase64()
+            return result
+        } catch {
+            print("Error detected while encrypting! \(error)")
+            throw error
+        }
+    }
+    
+    func decode(_ encodedData: String) throws -> String {
+        let data = Data(base64Encoded: encodedData)
+        
+        guard let data else { return "" }
+        
+        do {
+            let decoded = try getAESObject().decrypt(data.bytes)
+            let result = String(bytes: decoded, encoding: .utf8) ?? ""
+            
+            return result
+        } catch {
+            print("Error detected while encrypting! \(error)")
+            throw error
+        }
+    }
+    
+    private func getAESObject() throws -> AES {
+        do {
+            let aes = try AES(key: key, iv: iv, padding: .pkcs7)
+            return aes
+        } catch {
+            print("Can't get aes object")
+            throw error
+        }
+    }
+}
