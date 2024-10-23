ee2005cadde1bed8ce0f3f4400887676efdfd020
seungho <sts06017@naver.com>
2024-10-23 15:01:11 +0900

    [fix] 도움말 페이지 전체스크롤로 변경

--- ECO_FB_Test/ECO_FB_Test/View/EcoViews/HealthHelpView.swift
+++ ECO_FB_Test/ECO_FB_Test/View/EcoViews/HealthHelpView.swift
@@ -11,12 +11,15 @@ import HealthKit
 
 struct HealthHelpView: View {
     var body: some View {
-        Text("혹시 모든 정보가 0으로만 보이고, 변화가 없으신가요?")
-            .padding(.top, 50)
-        Text("설정 > 앱 > 건강 > 데이터 접근 및 기기 >\ne-co에서 '모두 켜기'를 눌러주세요.")
-            .padding()
-        
         ScrollView(.vertical) {
+            Text("혹시 모든 정보가 0으로만 보이고, 변화가 없으신가요?")
+                .padding(.top, 50)
+            Text("설정 > 앱 > 건강 > 데이터 접근 및 기기 >\ne-co에서 '모두 켜기'를 눌러주세요.")
+                .padding()
+            
+            Divider()
+                .padding()
+            
             Image(.healthHelp)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
