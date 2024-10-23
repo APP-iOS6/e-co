f61668d0c25291ca4b121c481b220e9404d08c25
seungho <sts06017@naver.com>
2024-10-23 14:25:26 +0900

    [fix] 앱 전환시에 updateUserPoints() 호출하도록 변경

--- ECO_FB_Test/ECO_FB_Test/View/EcoViews/EcoView.swift
+++ ECO_FB_Test/ECO_FB_Test/View/EcoViews/EcoView.swift
@@ -53,12 +53,12 @@ struct EcoView: View {
         .onChange(of: healthManager.todayStepCount) {
             print("onChange step")
             healthManager.loadData()
-            updateUserPoints()
+//            updateUserPoints()
         }
         .onChange(of: scene, {
             print("onChange scene")
             healthManager.loadData()
-//            updateUserPoints()
+            updateUserPoints()
         })
         .onAppear {
             print("onAppear")
